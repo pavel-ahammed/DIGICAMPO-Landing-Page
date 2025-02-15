d3ac10963cf546b9c51538d9c0b4f76db6c3b9b4
pavel-ahammed <pavelahammed68@gmail.com>
2025-02-16 01:26:34 +0600

    hero section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,12 @@ module.exports = {
           "linear-gradient(90deg, rgba(119,85,255,0.61) 0%, rgba(70,49,152,1) 100%);",
         heroLineargradient:
           "linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%)",
-        heroSecondlinearGradient:
-          "linear-gradient(90deg, rgba(231,234,253,1) 0%, rgba(235,233,253,1) 100%)",
+        letsTalklinearGradient:
+          "linear-gradient(90deg, rgba(119,85,255,1) 0%, rgba(144,85,255,1) 44%, rgba(255,195,16,1) 100%, rgba(234,227,255,1) 100%)",
+
         heroBackgroundimage: "url('/assets/nav-and-hero-svg/hero-bg.svg')",
+        letsTalkheroBackgroundimage:
+          "url('/assets/lets-talk-section-imge/lets-talk-section-image.png')",
         consultaion:
           "url('/assets/nav-and-hero-svg/consultaion-section-bg.svg')",
         connectUs: "url('assets/nav-and-hero-svg/connect-us-bg.svg')",
