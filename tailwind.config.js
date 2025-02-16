445ca0898d9dc42edbd539b2223b25dc41562c83
pavel-ahammed <pavelahammed68@gmail.com>
2025-02-17 02:44:47 +0600

    lets talk page complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,8 @@ module.exports = {
           "linear-gradient(90deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%)",
         letsTalklinearGradient:
           "linear-gradient(90deg, rgba(119,85,255,1) 0%, rgba(144,85,255,1) 44%, rgba(255,195,16,1) 100%, rgba(234,227,255,1) 100%)",
+        GraphBggradient:
+          "radial-gradient(circle, rgba(0,0,0,0.14889705882352944) 0%, rgba(0,0,0,0.1) 19%, rgba(0,0,0,0) 60%)",
 
         heroBackgroundimage: "url('/assets/nav-and-hero-svg/hero-bg.svg')",
         letsTalkheroBackgroundimage:
@@ -19,16 +21,24 @@ module.exports = {
         consultaion:
           "url('/assets/nav-and-hero-svg/consultaion-section-bg.svg')",
         connectUs: "url('assets/nav-and-hero-svg/connect-us-bg.svg')",
+        nextStepsectionBggraph:
+          "url('/assets/lets-talk-section-imge/next-step-section-graph.svg')",
+        nextStepsectionline:
+          "url('/assets/lets-talk-section-imge/next-step-section.svg')",
       },
       fontFamily: {
         fontPrimary: ["Inter", "serif"],
         fontSecondery: ["Lexend Deca", "serif"],
+        fontManrope: ["Manrope", "serif"],
       },
       borderColor: {
         btnBordercolor: "#7755FF",
       },
       colors: {
         navBtntextColor: "#7755FF",
+      },
+      boxShadow: {
+        stepsShadow: "0px 35px 50px 0px rgba(0,159,98,0.05)",
       },
     },
   },
